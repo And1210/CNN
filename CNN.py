ca7c29c6b9a8af1fc0f9ab711148474a6fe0787f
And1210 <amf7crazy@gmail.com>
2019-01-19 12:23:15 -0500

    CNN Skeleton

---
+++ CNN.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class Layer:
+    def __init__(self):
+        
+    def compute(self):
+        
+class CNN:
+    def __init__(self):
+        
+    def setLearningRate(self, newRate):
+        self.learningRate = newRate
+    def getLearningRate(self):
+        return self.learningRate
+        
+    def feedForward(self, inputData):
+    
+    def train(self, inputData, expectedOutput):
+        
