b350ab53afe377097bdf8d0fa357d8aaf31b3354
xdrgb22 <bunnysnow22@gmail.com>
2019-01-19 12:41:37 -0500

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -1,4 +1,19 @@
 import numpy as np
+import mnistdb.io as mio
+
+correctNumbers = mio.load() # array with the correct number
+data = mio.load(scaled=True) #the data of the images
+correctNumbersIndex = mio.load(one_hot=True) #array with a 1 at the index position of the correct number
+for i in range(60000):
+    trainingImagesArray = data.trainX[i] #the 60 000 training images
+    trainingImagesArray = np.array(trainingImagesArray,ndmin=2) #scaling the array so it has 2 dimensions
+    trainingIndexArray = correctNumbersIndex.trainY[i] # the index position of the correct number for the 60 000 training images
+for i in range(10000):
+    testingImagesArray = data.testX[i] #the 10 000 testing images
+    testingImagesArray = np.array(testingImagesArray,ndmin=2)
+    testingIndexArray = correctNumbersIndex.testY[i]
+
+
 
 class Layer:
     def __init__(self):
