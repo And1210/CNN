16e7d837372b0218e9200e813c07e03a62809e79
And1210 <amf7crazy@gmail.com>
2019-02-10 15:31:17 -0500

    Forward propogation almost works

--- CNN.py
+++ CNN.py
@@ -1,5 +1,6 @@
 import numpy as np
 import mnistdb.io as mio
+import skimage.measure
 
 correctNumbers = mio.load() # array with the correct number
 data = mio.load(scaled=True) #the data of the images
@@ -13,6 +14,15 @@ for i in range(10000):
     testingImagesArray = np.array(testingImagesArray,ndmin=2) #scaling the array so it has 2 dimensions
     testingIndexArray = correctNumbersIndex.testY[i] #the index position of the correct number for the 10 000 testing images
 
+#relu activation function
+def relu(image):
+    out = image
+    out[out<0] = 0
+    return out
+#max pooling function
+def maxPool(image, size):
+    skimage.measure.block_reduce(image, size, np.max)
+    return image
 
 class Filter:
     #filterSize - the size of the filter, the filter is square so it is one side length
@@ -77,7 +87,7 @@ class ConvLayer:
             counter = 0
             for i in range(len(imgs)):
                 for j in range(mult):
-                    images.push(imgs[counter])
+                    images.append(imgs[counter])
                 counter = counter + 1
         else:
             images = imgs
@@ -85,28 +95,79 @@ class ConvLayer:
             #Convolute old image with filter
             newImg = self.filters[i].convolute(images[i])
             #Apply activation function (ReLU)
-            
+            newImg = relu(newImg)
             #Pooling
-            
-            output.push(newImg)
+            newImg = maxPool(newImg, (2, 2))
+            output.append(newImg)
         return output
+
+#Each layer of the Neural Network has properties, those are stored here    
+class Layer:
+    #Layer constructor
+    #inputSize ----- the number of inputs (or neurons) from the previous layer
+    #selfSize ----- the number of neurons on this layer
+    def __init__(self, inputSize, selfSize):
+        self.weights = 2 * np.random.rand(selfSize, inputSize) - 1  #giving random weights to start
+        self.bias = 2 * np.random.rand(selfSize, 1) - 1 #giving random bias to start
+        
+    #Feeds an input vector through the layer to produce an output
+    #inputData ----- a 1D array or list of input values with same length as inputSize
+    def compute(self, inputData):
+        out = self.weights.dot(inputData)  #feeding the given inputs through the current layerout)
+        out = out + self.bias
+        out = 1 / (1 + np.exp(-out))    #sigmoid activation function
+        return out
         
 class CNN:
     #inputSize - tuple of the width and height of input image
     #hiddenLayers - list of number of neurons on each hidden layer
     #outputSize - the number of outputs (10 for mnist)
     #lr - the learning rate
-    def __init__(self, inputSize, hiddenLayers, outputSize, lr):
+    def __init__(self, inputSize, hiddenLayers, outputSize, lr=0.05):
         self.lr = lr
         self.layers = []
-        for neurons in hiddenLayers:
-            self.layers.push(ConvLayer(neurons, 3))
+        self.layerOut = [None for i in range(len(hiddenLayers) + 1)]
+        self.flattenOut = None
+        for neurons in hiddenLayers:    #Setting up the convolutional layers
+            self.layers.append(ConvLayer(neurons, 3))
+            
+#        flattenNum = int(inputSize[0]*inputSize[1]/(pow(4, len(hiddenLayers))))
+        testInput = [np.zeros(inputSize)] #getting the number of inputs for the flattened layer
+        for l in self.layers:
+            testInput = l.compute(testInput)
+        flattenNum = len(testInput) * testInput[0].shape[0] * testInput[0].shape[1]
+        self.finalLayer = Layer(flattenNum, outputSize)
+        
     def setLearningRate(self, newRate):
         self.learningRate = newRate
     def getLearningRate(self):
         return self.learningRate
         
     def feedForward(self, inputData):
-        pass
+        curData = [inputData]
+        index = 0
+        for l in self.layers:
+            curData = l.compute(curData)
+            self.layerOut[index] = curData
+            index = index + 1
+        curData = np.array(curData).flatten()
+        self.flattenOut = curData
+        probabilities = self.finalLayer.compute(curData)
+        return probabilities
+    
     def train(self, inputData, expectedOutput):
-        pass
+        curOutput = self.feedForward(inputData)
+        cost = expectedOutput - curOutput
+        layerOutputs = [inputData] + self.layerOut
+        
+        #train the fully connected layer at the end
+        gradient = self.lr * cost * curOutput * (1 - curOutput)
+        weightChange = gradient.dot(self.flattenOut.transpose())
+        self.finalLayer.weights = self.finalLayer.weights + weightChange
+        self.finalLayer.bias = self.finalLayer.bias + gradient
+#        for i in range(len(layerOutputs)):
+            
+
+a = np.zeros((100, 100))
+nn = CNN(a.shape, [2, 4], 2)
+out = nn.feedForward(a)
