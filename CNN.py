efc6b95942c1a32c20acf8acfec66925c5a0d237
DuncanMays <44309391+DuncanMays@users.noreply.github.com>
2019-02-10 15:04:16 -0500

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -1,6 +1,5 @@
 import numpy as np
 import mnistdb.io as mio
-import skimage.measure
 
 correctNumbers = mio.load() # array with the correct number
 data = mio.load(scaled=True) #the data of the images
@@ -13,52 +12,62 @@ for i in range(10000):
     testingImagesArray = data.testX[i] #the 10 000 testing images
     testingImagesArray = np.array(testingImagesArray,ndmin=2) #scaling the array so it has 2 dimensions
     testingIndexArray = correctNumbersIndex.testY[i] #the index position of the correct number for the 10 000 testing images
-#relu activation function
-def relu(self, image):
-    for r in numpy.arange(0,image[0]):
-        for c in numpy.arange(0, image[1]):
-            if image[r, c] > 0:
-                image[r, c] = image[r, c]
-            else:
-                image[r, c] = 0
-    return image
-#max pooling function
-def maxPool(self, image, size):
-    skimage.measure.block_reduce(image, size, np.max)
-    return image
+
 
 class Filter:
     #filterSize - the size of the filter, the filter is square so it is one side length
+    #***filter size must be odd***
     def __init__(self, filterSize):
-        self.values = np.random.uniform(low=-1, high=1, size=(filterSize, filterSize))
-        self.w = filterSize
+    	if(filterSize%2 == 0):
+    		raise Exception("filterSize [the only parameter to Filter.__init__()] must be odd")
+    	else:
+        	self.values = np.random.uniform(low=-1, high=1, size=(filterSize, filterSize))
+        	self.size = filterSize
+        
+    #this function convolutes the filter over the given image
+    #a is the filter b is the image under convolution
+    def convolute(self, b):
 
-    #an image represented as a 2D numpy array
-    def convolute(self, image):
-        #this is where the filter is convoluted over the entire input image
-        #should produce a new image with features highlighted
+    	#the "kernel" of convolution
+       	a = self.values
 
-    #relu activation function
-    def relu(self, image):
-        for r in numpy.arange(0,image[0]):
-            for c in numpy.arange(0, image[1]):
-                if image[r, c] > 0:
-                    image[r, c] = image[r, c]
-                else:
-                    image[r, c] = 0
-        return image
-    #max pooling function
-    def maxPool(self, image, size):
-        skimage.measure.block_reduce(image, size, np.max)
-        return image
+       	#each sum is multiplied by this number to "normalize" it
+       	normalizationConstant = 1/((self.size)**2)
 
+       	#this will be the output of the function, it's the array where all the convolution sums in the below for loop will end up 
+       	#instead of initializing an array of the same dimensions as b, I just copied all the indexes over. quick and dirty i know
+       	output = b[:]
+
+       	#the use of this variable will become clear later, basically it makes the convolution sum to start at one corner of the kernel
+       	shift = int((self.size-1)/2)
+
+       	#x and y are the indices of convolution, ie, they're the indices of the cell that the current sum being calculated will end up
+       	for x in range(0, len(b)):
+       		#it is necessary that b is at least a rectangular matrix so that b[0] is the same length as b[q] for any index q
+       		for y in range(0, len(b[0])):
+       			output[x][y] = 0
+
+       			#these for loops iterate over the kernel and the corresponding indices in the image
+       			for i in range(0, self.size):
+       				for j in range(0, self.size):
+       					try:
+       						#see how the shift variable makes the convolution sum to start at one corner of the kernel
+       						output[x][y] += a[x+shift-i][x+shift-j]*b[y+shift-i][x+shift-j]
+       					except IndexError:
+       						#if the kernel overlaps the edge of b, consider the value of b[y+shift-i][x+shift-j] to be zero
+       						output[x][y] += 0
+       			#"normalizes" the output
+       			output[x][y] *= normalizationConstant
+
+       	return output
+        
 #Convolution layer
 class ConvLayer:
     #neuronNum - the number of neurons on the layer
     #filterSize - the size of the filter, the filter is square so it is one side length
     def __init__(self, neuronNum, filterSize):
         self.filters = [Filter(filterSize) for x in range(neuronNum)]
-
+        
     #imgs - a list of input images to the layer
     def compute(self, imgs):
         output = []
@@ -76,78 +85,28 @@ class ConvLayer:
             #Convolute old image with filter
             newImg = self.filters[i].convolute(images[i])
             #Apply activation function (ReLU)
-            newImg = relu(newImg)
+            
             #Pooling
-            newImg = maxPool(newImg)
+            
             output.push(newImg)
         return output
-
-#Each layer of the Neural Network has properties, those are stored here
-class Layer:
-    #Layer constructor
-    #inputSize ----- the number of inputs (or neurons) from the previous layer
-    #selfSize ----- the number of neurons on this layer
-    def __init__(self, inputSize, selfSize):
-        self.weights = 2 * np.random.rand(selfSize, inputSize) - 1  #giving random weights to start
-        self.bias = 2 * np.random.rand(selfSize, 1) - 1 #giving random bias to start
-
-    #Feeds an input vector through the layer to produce an output
-    #inputData ----- a 1D array or list of input values with same length as inputSize
-    def compute(self, inputData):
-        out = self.weights.dot(inputData)  #feeding the given inputs through the current layerout)
-        out = out + self.bias
-        out = 1 / (1 + np.exp(-out))    #sigmoid activation function
-        return out
-
+        
 class CNN:
     #inputSize - tuple of the width and height of input image
     #hiddenLayers - list of number of neurons on each hidden layer
     #outputSize - the number of outputs (10 for mnist)
     #lr - the learning rate
-    def __init__(self, inputSize, hiddenLayers, outputSize, lr=0.05):
+    def __init__(self, inputSize, hiddenLayers, outputSize, lr):
         self.lr = lr
         self.layers = []
-        self.layerOut = [None for i in range(hiddenLayers + 1)]
-        self.flattenOut = None
-        for neurons in hiddenLayers:    #Setting up the convolutional layers
-            self.layers.append(ConvLayer(neurons, 3))
-
-#        flattenNum = int(inputSize[0]*inputSize[1]/(pow(4, len(hiddenLayers))))
-        testInput = np.zeros(inputSize) #getting the number of inputs for the flattened layer
-        for l in self.layers:
-            testInput = l.compute(testInput)
-        flattenNum = testInput.shape[0] * testInput.shape[1]
-        self.finalLayer = Layer(flattenNum, outputSize)
-
+        for neurons in hiddenLayers:
+            self.layers.push(ConvLayer(neurons, 3))
     def setLearningRate(self, newRate):
         self.learningRate = newRate
     def getLearningRate(self):
         return self.learningRate
-
+        
     def feedForward(self, inputData):
-        curData = inputData
-        index = 0
-        for l in self.layers:
-            curData = l.compute(curData)
-            self.layerOut[index] = curData
-            index = index + 1
-        curData = curData.flatten()
-        self.flattenOut = curData
-        probabilities = self.finalLayer.compute(curData)
-        return probabilities
-
+        pass
     def train(self, inputData, expectedOutput):
-        curOutput = self.feedForward(inputData)
-        cost = expectedOutput - curOutput
-        layerOutputs = [inputData] + self.layerOut
-
-        #train the fully connected layer at the end
-        gradient = self.lr * cost * curOutput * (1 - curOutput)
-        weightChange = gradient.dot(self.flattenOut.transpose())
-        self.finalLayer.weights = self.finalLayer.weights + weightChange
-        self.finalLayer.bias = self.finalLayer.bias + gradient
-#        for i in range(len(layerOutputs)):
-
-
-a = np.zeros((100, 100))
-nn = CNN(a.shape, [2, 4], 2)
+        pass
